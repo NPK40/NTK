0971edc37f848c1487963fe62a6e3802aaa96945
Mortefromplanescape <boreich89@gmail.com>
2024-11-11 08:58:13 +0300

    Update HomeController.cs

--- pesok.pro/pesok.pro/Controllers/HomeController.cs
+++ pesok.pro/pesok.pro/Controllers/HomeController.cs
@@ -18,6 +18,13 @@ namespace pesok.pro.Controllers
             return View();
         }
 
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
