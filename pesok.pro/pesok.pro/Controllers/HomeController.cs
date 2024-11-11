8cefd7312858daa89757e57b6f8079561da3ddf1
NPK40 <npk40@yandex.ru>
2024-11-11 21:46:46 +0300

    Страница цены

--- pesok.pro/pesok.pro/Controllers/HomeController.cs
+++ pesok.pro/pesok.pro/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace pesok.pro.Controllers
             return View();
         }
 
+        public IActionResult Price()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
